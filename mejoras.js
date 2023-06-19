96b87dd56427f948f6cbe39811da44c5e48f2c29
alexis206 <137022662+alexis206@users.noreply.github.com>
2023-06-19 01:33:23 -0300

    mejoras.js

--- mejoras.js
+++ mejoras.js
@@ -48,6 +48,9 @@ let meGustaBtn = document.getElementById("meGustaBtn");
       contadorClics++;
       contador.textContent = contadorClics;
 
+      if (contadorClics === 5) {
+        alert("Es para probar si funciona");
+      }
       if (contadorClics === 9) {
         alert("Si seguis tocando el boton sos pt");
       }
@@ -57,5 +60,19 @@ let meGustaBtn = document.getElementById("meGustaBtn");
       if (contadorClics === 20) {
         alert("Ah pero sos alto...");
       }
+      if (contadorClics === 40) {
+        alert("Como te gusta cogote");
+      }
+      if (contadorClics === 60) {
+        alert("No hay mas textos despues de este");
+      }
+      if (contadorClics === 80) {
+        alert("No hay mas textos despues de este");
+      }if (contadorClics === 90) {
+        alert("...");
+      
+      }if (contadorClics === 100) {
+      alert("re al pedo estaba el tipal");
+    }
     });
 });
