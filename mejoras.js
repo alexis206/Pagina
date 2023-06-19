ea8dabd74b6bb9af8ec9e27dde09c58516d4ec05
alexis206 <137022662+alexis206@users.noreply.github.com>
2023-06-19 02:17:22 -0300

    mejoras.js

--- mejoras.js
+++ mejoras.js
@@ -68,53 +68,53 @@ let meGustaBtn = document.getElementById("meGustaBtn");
       }
       if (contadorClics === 80) {
         alert("Cuanto tiempo podrias seguir haciendo esto?");
-      }if (contadorClics === 90) {
+      if (contadorClics === 90) {
         alert("no vas a parar?");
       
-      }if (contadorClics === 100) {
+      if (contadorClics === 100) {
       alert("re al pedo estaba el tipal");
     }
-       }if (contadorClics === 120) {
+      }if (contadorClics === 120) {
       alert("me cuesta menos a mi que a vos");
     }
-       }if (contadorClics === 140) {
+       if (contadorClics === 140) {
       alert("anda a apretar el boton enviar");
     }
-       }if (contadorClics === 160) {
+       if (contadorClics === 160) {
       alert("dale pelotudo");
     }
-       }if (contadorClics === 180) {
+       if (contadorClics === 180) {
       alert("naaa cualquiera");
     } 
-      }if (contadorClics === 200) {
+      if (contadorClics === 200) {
       alert("bueno... como veo que no vas a parar, cada 100 clicks voy a poner un texto");
     } 
-      }if (contadorClics === 300) {
+      if (contadorClics === 300) {
       alert("esto no es nada productivo para tu vida");
     } 
-      }if (contadorClics === 400) {
+      if (contadorClics === 400) {
       alert("sabias que si supieras manejar la consola podrias ver todos los textos que siguen?");
     } 
-     }if (contadorClics === 500) {
+     if (contadorClics === 500) {
       alert("pero como sos pajero vas a seguir haciendo click");
     } 
-      }if (contadorClics === 600) {
+      if (contadorClics === 600) {
       alert("el ultimo mensaje te espera en los 1000");
     } 
-      }if (contadorClics === 1000) {
+      if (contadorClics === 1000) {
       alert("sera que hay mas mensajes despues de este?");
     } 
-      }if (contadorClics === 1100) {
+      if (contadorClics === 1100) {
       alert("muy pocas personas se atreverian a tanto");
     } 
-      }if (contadorClics === 1200) {
+      if (contadorClics === 1200) {
       alert("bueno ahora si, anda a dormir");
     } 
-      }if (contadorClics === 1500) {
+      if (contadorClics === 1500) {
       alert("no puede ser...");
     } 
-     }if (contadorClics === 2000) {
+     if (contadorClics === 2000) {
       alert("Ganaste un naranja");
     }  
-    });
+      }});
 });
